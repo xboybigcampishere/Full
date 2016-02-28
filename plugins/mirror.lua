163efe3388c7dc594f98eb0d12d048f315852624
pouyaxboy <offboy098@gmail.com>
2016-02-28 10:10:06 -0800

    Create mirror.lua

---
+++ plugins/mirror.lua
@@ -0,0 +1,37 @@
+do
+
+function run(msg, matches)
+  return [[Mirror TG V2😎
+special thanks to:
+yagop😊
+and〰
+seedteam😊
+
+Super Group Bot😵
+
+Sudo😎: @pouya_x_boy
+MIRROR TG IS You'r Super Group and group manager👂😎
+
+  ]]
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "#Mirror : return Bot help",
+  patterns = {
+   [#]Mirror`
+  },
+  run = run
+}
+end
+--Open source Plugin
+--channel: @MirrorTGch
+--Sudo: @pouya_x_boy
+--Mirror TG Is You'r super group and group Manager
+--قیمت گپ ها
+--سوپر گروه ماهانه 2 هزار تومان همیشگی 5 هزار تومان
+--گروه معمولی همیشگی 2 هزار تومان
+--برای ساخت گپ به آیدی زییر پیام بدید
+--@pouya_x_boy
+--دوستانی که ریپورت هستند هم به ربات زیر پیام دهند پیام هاتون به دستم میرسه و جواب هم از طریق ربات میدم
+--@pouya_x_boy_bot
