743fc77b011922df6b06f6ed924622dee8b9a59d
pouyaxboy <offboy098@gmail.com>
2016-02-28 10:09:02 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,9 +14,7 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
-  --GNU GPL v2 license.]]
+  return 'V2
 end
 
 return {
